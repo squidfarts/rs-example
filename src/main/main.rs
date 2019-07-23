fd9a84c405575db333a9c9b2a6344a031ec12191
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 16:19:17 -0700

    Add files via upload

---
+++ src/main/main.rs
@@ -0,0 +1,26 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.rs                                                                   *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: MIT                                                                    *
+ *                                                                                 *
+ ***********************************************************************************/
+
+
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, Rust' is desplayed to the standerd console.
+ *
+ */
+fn main()
+{
+    print!("Hello, Rust.");
+    
+}// end of function main
